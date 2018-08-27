3faa7664c8614e21fa2b580503710c30c04d8f25
Shinya Kumagai <roomful.rooms@gmail.com>
2018-08-27 14:26:23 +0900

    Support Android Pie(9.0)

--- plugin/src/test/kotlin/com/github/droibit/plugin/androidapilevel/model/AndroidApiReaderTest.kt
+++ plugin/src/test/kotlin/com/github/droibit/plugin/androidapilevel/model/AndroidApiReaderTest.kt
@@ -11,7 +11,7 @@ class AndroidApiReaderTest {
     fun readAndroidApisJson() {
         val androidApis = AndroidApiReader.readFromJson(ANDROID_API_JSON_PATH)!!
 
-        assertThat(androidApis.raw).hasLength(27)
+        assertThat(androidApis.raw).hasLength(28)
     }
 
     @Test
